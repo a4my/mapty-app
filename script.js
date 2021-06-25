0709b690b03af13462fde1c19a0e8f5822a1b321
Alex Fourmy <alexfourmy@gmail.com>
2021-06-25 09:13:35 +0200

    Created  running and cycling classes and refactored the parent class

--- script.js
+++ script.js
@@ -3,6 +3,50 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+class Workout {
+    date = new Date()
+    id = (Date.now() + '').slice(-10)
+
+    constructor(coords, distance, duration) {
+        this.coords = coords
+        this.distance = distance
+        this.duration = duration
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration)
+        this.cadence = cadence
+        this.calcPace()
+    }
+
+    calcPace() {
+        this.pace = this.duration / this.distance
+        return this.pace
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration)
+        this.elevationGain = elevationGain
+        this.calcSpeed()
+    }
+
+    calcSpeed() {
+        this.speed = this.distance / (this.duration / 60)
+        return this.speed
+    }
+}
+
+const run1 = new Running([39 -12], 5.2, 24, 178)
+console.log(run1)
+
+
+/////////////////////////////////
+// APPLICATION ARCHITECTURE
+
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,57 +55,99 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class App {
+    #map;
+    #mapEvent;
+    constructor() {
+        this._getPosition()
+        form.addEventListener('submit', this._newWorkout.bind(this))
 
-if(navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(
-        function(position) {
-            const { latitude } = position.coords
-            const { longitude } = position.coords
-            console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+        inputType.addEventListener('change', this._toggleElevationField)
+    }
 
-            const coords = [latitude, longitude]
-            map = L.map('map').setView(coords, 13);
+    _getPosition() {
+        if(navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(
+            this._loadMap.bind(this),
+            function() {
+                alert('Could not get your position')
+        })
+    }
 
-            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+    _loadMap(position) {
+        const { latitude } = position.coords
+        const { longitude } = position.coords
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
 
-            // Handling clicks on map
-            map.on('click', function(mapE) {
-                mapEvent = mapE
-                form.classList.remove('hidden')
-                inputDistance.focus()
-            })
-    }, function() {
-        alert('Could not get your position')
-})
+        const coords = [latitude, longitude]
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault()
+        console.log(this)
+        this.#map = L.map('map').setView(coords, 13);
 
-    // Clear input fields
-    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
 
-    // Display marker
-    console.log(mapEvent)
-    const {lat, lng} = mapEvent.latlng
+        // Handling clicks on map
+        this.#map.on('click', this._showForm.bind(this))
+    }
 
-    L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-        L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-    }))
-    .setPopupContent('Workout')
-    .openPopup();
-})
+    _showForm(mapE) {
+        this.#mapEvent = mapE
+        form.classList.remove('hidden')
+        inputDistance.focus()
+    }
 
-inputType.addEventListener('change', function() {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
-})
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    }
+
+    _newWorkout(e) {
+        e.preventDefault()
+
+        // get data from form
+        const type = inputType.value
+        const distance = +inputDistance.value
+        const duration = +inputDuration.value
+        
+        // if workout is running, create a running object
+        if(type === 'running') {
+            // Check if data is valid
+            const cadence = +inputCadence.value
+        }
+        // If workout is cycling, create a cycling object
+        if(type === 'cycling') {
+            const elevation = +inputElevation.value
+        }
+        // Add a new object to workout array
+
+        // Render workout on map as marker
+        const {lat, lng} = this.#mapEvent.latlng
+
+        L.marker([lat, lng])
+        .addTo(this.#map)
+        .bindPopup(
+            L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+        }))
+        .setPopupContent('Workout')
+        .openPopup();
+
+        // Render workout on list
+
+        // Clear input fields
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+
+        // Display marker
+
+    }
+}
+
+const app = new App()
+
+
