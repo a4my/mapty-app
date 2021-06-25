35b644b3cf9ab1e1aa49f44317af82e770e5ca20
Alex Fourmy <alexfourmy@gmail.com>
2021-06-25 10:58:58 +0200

    Completed the move to marker on click feature

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 class Workout {
     date = new Date()
     id = (Date.now() + '').slice(-10)
+    clicks = 0
 
     constructor(coords, distance, duration) {
         this.coords = coords
@@ -14,6 +15,10 @@ class Workout {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
+    }
+
+    click() {
+        this.clicks++
     }
 }
 
@@ -66,6 +71,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
     #map;
+    #mapZoomLevel = 13;
     #mapEvent;
     #workouts = []
 
@@ -74,6 +80,7 @@ class App {
         form.addEventListener('submit', this._newWorkout.bind(this))
 
         inputType.addEventListener('change', this._toggleElevationField)
+        containerWorkouts.addEventListener('click', this._moveToPopup.bind(this))
     }
 
     _getPosition() {
@@ -239,6 +246,28 @@ class App {
         `
 
         form.insertAdjacentHTML('afterend', html)
+    }
+
+    _moveToPopup(e) {
+        const workoutEl = e.target.closest('.workout')
+        console.log(workoutEl)
+
+        if(!workoutEl) return
+
+        const workout = this.#workouts.find(
+            work => work.id === workoutEl.dataset.id
+        )
+        console.log(workout)
+
+        this.#map.setView(workout.coords, this.#mapZoomLevel,{
+            animate: true,
+            pan: {
+                duration: 1,
+            }
+        })
+
+        // Using the public interface
+        workout.click()
     }
 }
 
